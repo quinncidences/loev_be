9100d492931ee8d8b38213f8db6c204666ccf2cb
quinncidences <qrc.inpa@gmail.com>
2019-06-06 11:46:29 -0700

    added routes for user, preference, car

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,3 +1,4 @@
 class Preference < ApplicationRecord
+  validates :relationship, :distance, :gender, :user_id, presence: true
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :cars
   has_one :preference
+
+  validates :first_name, :last_name, :email, :password, :tagline, :bio, :dob, :gender, :current_location, presence: true
+  validates :email, uniqueness: true
 end
