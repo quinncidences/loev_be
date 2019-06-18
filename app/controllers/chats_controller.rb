16825c64583a1b8e061b85d20516b4f628355e19
quinncidences <qrc.inpa@gmail.com>
2019-06-17 17:08:38 -0700

    spelling change for chats controller

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,10 +1,10 @@
 class ChatsController < ApplicationController
 
   def create
-    @chat1 = Chat.new(used_id: params[:user_id], recipient_id: params[:recipient_id], recipient_name: params[:recipient_name])
+    @chat1 = Chat.new(user_id: params[:user_id], recipient_id: params[:recipient_id], recipient_name: params[:recipient_name])
     @chat1.save!
 
-    @chat2 = Chat.new(used_id: params[:recipient_id], recipient_id: params[:user_id], recipient_name: params[:user_name])
+    @chat2 = Chat.new(user_id: params[:recipient_id], recipient_id: params[:user_id], recipient_name: params[:user_name])
     @chat2.save!
     render json: {chat1: @chat1, chat2: @chat2}
   end
