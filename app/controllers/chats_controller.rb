b5baa2639e205262da663b1af73028977cd8373f
quinncidences <qrc.inpa@gmail.com>
2019-06-18 10:52:47 -0700

    adding back in the chat serializer and Show route for Chat

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,5 +1,15 @@
 class ChatsController < ApplicationController
 
+  def index
+    @chats = Chat.all
+    render json: @chats
+  end
+
+  def show
+    @chat = Chat.find(params[:id])
+    render json: @chat
+  end
+
   def create
     @chat1 = Chat.new(user_id: params[:user_id], recipient_id: params[:recipient_id], recipient_name: params[:recipient_name])
     @chat1.save!
---
+++ app/serializers/chat_serializer.rb
@@ -0,0 +1,4 @@
+class ChatSerializer < ActiveModel::Serializer
+  attributes :id, :messages
+
+end
